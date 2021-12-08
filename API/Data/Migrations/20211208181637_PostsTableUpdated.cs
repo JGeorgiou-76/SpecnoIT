54151131e717350197ba82cc8d71f4896769e67d
JGeorgiou-76 <tonykart13@gmail.com>
2021-12-08 20:19:01 +0200

    Posts Table Updated

---
+++ API/Data/Migrations/20211208181637_PostsTableUpdated.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class PostsTableUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Posts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PublicId",
+                table: "Posts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
